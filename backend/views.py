0446a77bd928ea8a3c27edd3cddfad078d5008f1
Mohammed Ali <mohammedaalli088@gmail.com>
2023-12-24 19:23:44 +0000

    Desinging the home page

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View 
 
 # Create your views here.
+
+class HomePageView(View):
+  def get(self, request, *args, **kwargs):
+    return render(request, 'main/index.html')
+    
