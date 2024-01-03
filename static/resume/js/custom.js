b88d1977c64059591ee186489f4c64f52ffa5087
Mohammed Ali <mohammedaalli088@gmail.com>
2024-01-03 10:40:26 +0000

    still in progress

--- static/resume/js/custom.js
+++ static/resume/js/custom.js
@@ -2,16 +2,16 @@
 
 // PRELOADER
 
-$(window).load(function(){
-    $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
-});
+// $(window).load(function(){
+//     $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+// });
 
-$(function(){
-    jQuery(document).ready(function() {
-		$('body').backstretch([
-	 		 "{% static 'resume/images/tm-bg-slide-1.jpg' %}", 
-	 		 "{% static 'resume/images/tm-bg-slide-2.jpg' %}",
-			 "{% static 'resume/images/tm-bg-slide-3.jpg' %}",
-		], {duration: 3200, fade: 1300});
-	});
-})
+// $(function(){
+//     jQuery(document).ready(function() {
+// 		$('body').backstretch([
+// 	 		 "{% static '../images/tm-bg-slide-1.jpg' %}", 
+// 	 		 "{% static '../images/tm-bg-slide-2.jpg' %}",
+// 			 "{% static '../images/tm-bg-slide-3.jpg' %}",
+// 		], {duration: 3200, fade: 1300});
+// 	});
+// })
