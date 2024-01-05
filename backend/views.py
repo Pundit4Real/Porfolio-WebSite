aad8baee850692009ac8fa514dda05cd69711d0c
Mohammed Ali <mohammedaalli088@gmail.com>
2024-01-05 05:24:47 +0000

    coding the view for contact me functionality

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View 
+from django.core.mail import send_mail
+from django.contrib import messages
 from .forms import ContactMeForm
 
 # Create your views here.
@@ -12,3 +14,18 @@ class HomePageView(View):
 class ResumeView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'linked-pages/resume.html')
+    
+def contact(request):
+   if request.method == 'POST':
+      form = ContactMeForm(request.POST)
+      if form.is_valid():
+         form.cleaned_data['name']
+         form.cleaned_data['email']
+         form.cleaned_data['subject']
+         form.cleaned_data['message']
+
+         full_message = f"Message from: {name}\nEmail: {email}\n\n{message}"
+            
+         send_mail(subject, full_message, email, ['your_email@example.com'])
+
+
