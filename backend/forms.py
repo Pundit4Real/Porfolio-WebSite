8ac7dd25b935cce93de1f17c51da16dc4560da2e
Mohammed Ali <mohammedaalli088@gmail.com>
2024-01-05 05:51:34 +0000

    coding the view for contact me functionality

--- backend/forms.py
+++ backend/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 
 class ContactMeForm(forms.Form):
-    name = forms.CharField(_("Name"), max_length=100, widget=forms.TextInput(
+    name = forms.CharField(max_length=100, widget=forms.TextInput(
                             attrs={'class':'form-control','placeholder':'Your name....','required':True}))
-    email = forms.EmailField(_("Email"), widget=forms.TextInput(
+    email = forms.EmailField(widget=forms.TextInput(
                             attrs={'class':'form-control', 'placeholder':'Your email.....','required':True}))
-    subject = forms.CharField(_("Subject"), max_length=70,widget=forms.TextInput(
+    subject = forms.CharField(max_length=70,widget=forms.TextInput(
                                 attrs={'class':'form-control','placeholder':'Subject.....','required':True}))
-    message = forms.CharField(_("Message"),widget=forms.Textarea(
+    message = forms.CharField(widget=forms.Textarea(
                             attrs={'class':'form-control','row':5,'placeholder':'Your message......','required':True}))
