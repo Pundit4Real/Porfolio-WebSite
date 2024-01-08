8488c59783f5e12ac80c2b8c435e156c20f94f56
Mohammed Ali <mohammedaalli088@gmail.com>
2024-01-08 08:03:30 +0000

    forms refactored

--- backend/forms.py
+++ backend/forms.py
@@ -1,10 +1,40 @@
 from django import forms
 
 class ContactMeForm(forms.Form):
-    name = forms.CharField(max_length=100, widget=forms.TextInput(
-                            attrs={'class':'form-control','placeholder':'Your name....','required':True}))
-    email = forms.EmailField(widget=forms.TextInput(
-                            attrs={'class':'form-control', 'placeholder':'Your email.....','required':True}))
-    subject = forms.CharField(max_length=70,widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Subject.....','required':True}))
-    message = forms.CharField(widget=forms.Textarea(
-                            attrs={'class':'form-control','row':5,'placeholder':'Your message......','required':True}))
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class':        'form-control',
+                'placeholder':  'Your name....',
+                'required':     True
+                }
+            )
+        )
+    
+    email = forms.EmailField(
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control', 
+                'placeholder':'Your email.....',
+                'required':True
+                }
+            )
+        )
+    subject = forms.CharField(
+        max_length=70,
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Subject.....',
+                'required':True
+                }
+            )
+        )
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class':'form-control',
+                'row':5,
+                'placeholder':'Your message......',
+                'required':True}))
