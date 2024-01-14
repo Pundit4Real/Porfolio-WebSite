1f05271743504771be3d48d0c0d81d61ab70cb6e
Mohammed Ali <mohammedaalli088@gmail.com>
2024-01-14 07:06:57 +0000

    configuration for deploying on railway

--- porfolio/settings.py
+++ porfolio/settings.py
@@ -78,17 +78,13 @@ WSGI_APPLICATION = 'porfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-
-        'ENGINE': os.getenv('ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
