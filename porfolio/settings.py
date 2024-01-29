682f5f4f7fec9520799e5e719c97b067f3db38e6
Mohammed Ali <mohammedaalli088@gmail.com>
2024-01-29 23:46:29 +0000

    Ref: settings file refactored

--- porfolio/settings.py
+++ porfolio/settings.py
@@ -120,7 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
