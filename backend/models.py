014c5b9a8786c94fcad1985dc229aca413b0be49
Mohammed Ali <mohammedaalli088@gmail.com>
2024-04-12 10:07:54 +0000

    ref: index template updated

--- backend/models.py
+++ backend/models.py
@@ -6,3 +6,15 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     name = models.CharField(max_length=100, default="")
 
+class AboutMe(models.Model):
+    intro_img = models.ImageField(upload_to='about/')
+    intro_text_title = models.CharField(max_length=110)
+    intro_text = models.TextField(max_length=400,null=True, blank=True)
+    #skills 
+    skills_img = models.ImageField(upload_to='about/')
+    skills_text_title = models.CharField(max_length=110)
+    skills_text = models.TextField(max_length=500,null=True, blank=True)
+
+class Projects(models.Model):
+    ...
+
