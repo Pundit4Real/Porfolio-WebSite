48f56c6e964912ba881cc8d8563f246df84c63ea
Mohammed Ali <mohammedaalli088@gmail.com>
2024-04-12 22:14:18 +0000

    feat: models models created

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from backend.models import Profile, AboutMe,Projects,Works
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Projects)
+admin.site.register(AboutMe)
+admin.site.register(Works)
 
--- backend/models.py
+++ backend/models.py
@@ -6,7 +6,15 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     name = models.CharField(max_length=100, default="")
 
+    class meta:
+        verbose_name_plural = "Profile"
+
+    def __str__(self):
+        return self.name
+
+
 class AboutMe(models.Model):
+    header = models.CharField(max_length=100)
     intro_img = models.ImageField(upload_to='about/')
     intro_text_title = models.CharField(max_length=110)
     intro_text = models.TextField(max_length=400,null=True, blank=True)
@@ -15,6 +23,36 @@ class AboutMe(models.Model):
     skills_text_title = models.CharField(max_length=110)
     skills_text = models.TextField(max_length=500,null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = 'About Me'
+
+    def __str__(self):
+        return self.header
+
 class Projects(models.Model):
-    ...
+    cartegory = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return self.cartegory
+
+
+class Works(models.Model):
+    cartegory = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, null=True,blank=True)
+    image = models.ImageField(upload_to='works/')
+    link = models.URLField(max_length=200)
+
+
+    class Meta:
+        verbose_name_plural = "Works"
+
+
+    def __str__(self):
+        return self.title
+
+    
+
 
