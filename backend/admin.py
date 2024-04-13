a995086ae3e3c24b1d9a4873c126ec69d122ad07
Mohammed Ali <mohammedaalli088@gmail.com>
2024-04-13 05:42:24 +0000

    feat: additional models added

--- backend/admin.py
+++ backend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from backend.models import (Profile, AboutMe,Projects,Works,
+from backend.models.models import (Profile, AboutMe,Projects,Works,
                             ProjectPorfolio)
 # Register your models here.
 
